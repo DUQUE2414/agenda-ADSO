295e98cf230da8fd2ce6ae3403bb40be24af18bb
DUQUE2414 <danielduagu1@gmail.com>
2026-07-22 19:55:26 -0500

    mi primera web subida en react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA</h1>
+ <h2>La fecha es {fecha}</h2>
+ <p style = {{color: "black"}}>Soy Daniel Duque aprendiz SENA de la tecnologia analisis y desarrollo de software</p>
+ <p>Cr7 Siuuuuuu</p>
+ </main>)}
