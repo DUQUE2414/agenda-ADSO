67c35c6fca3df7398164419526a90b5bbbd02de4
DUQUE2414 <danielduagu1@gmail.com>
2026-08-05 12:49:40 -0500

    otro cambio

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,5 @@ return (
  <main>
  <h1>Hola SENA</h1>
  <h2>La fecha es {fecha}</h2>
- <p style = {{color: "black"}}>Soy Daniel Duque aprendiz SENA de la tecnologia analisis y desarrollo de software</p>
+ <p style = {{color: "black"}}>Soy Daniel Duque aprendiz SENA tecnologo en analisis y desarrollo de software</p>
  </main>)}
