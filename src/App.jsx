44d3a031fcd3edbc4dff057b926d11b6afcbd10f
DUQUE2414 <danielduagu1@gmail.com>
2026-08-19 17:06:33 -0500

    Clase_6_Tailwind_UI_Agenda_ADSO_v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import FormularioContacto from "./components/FormularioContacto";
 import ContactoCard from "./components/ContactoCard";
 
@@ -25,17 +24,31 @@ export default function App() {
   };
 
   return (
-    <main className="app-container">
-      <h1 className="app-title">Agenda ADSO v3</h1>
-      <p className="subtitulo">Persistencia con localStorage + UI moderna</p>
+    <div className="max-w-4xl mx-auto">
+      <main className="min-h-screen py-10 px-4">
 
-      <FormularioContacto onAgregar={agregarContacto} />
+        {  /* Título centrado con color morado */}
+        <h1 className="text-4xl font-bold text-center text-purple-600 mb-8">
+          Agenda ADSO v3
+        </h1>
 
-      <section className="lista-contactos">
-        {contactos.map((c) => (
-          <ContactoCard key={c.id} {...c} onEliminar={eliminarContacto} />
-        ))}
-      </section>
-    </main>
+
+        {/* Tarjeta del formulario */}
+        <section className="bg-white border border-gray-200 rounded-xl shadow-sm p-6 mb-6">
+          <FormularioContacto onAgregar={agregarContacto} />
+        </section>
+        {/* Lista de contactos */}
+        <section className="space-y-4">
+          {contactos.map((c) => (
+            <ContactoCard
+              key={c.id}
+              {...c}
+              onEliminar={eliminarContacto}
+            />
+          ))}
+        </section>
+      </main>
+    </div>
+
   );
 }
